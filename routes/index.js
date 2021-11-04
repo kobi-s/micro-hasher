660a0e673fcde454c3a027414a3d3861d5fb5aa1
Kobi <shotrz@gmail.com>
2021-11-04 20:11:29 +0200

    fix path of log file

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,11 @@ const router = require('express').Router()
 router.use("/ping", (req, res) => {
     return res.send('pong')
 })
-
 router.get("/log", (req, res) => {
     return res.download(__dirname + '../micro-hasher-process.log')
 })
-
 router.get("/cracked.txt", (req, res) => {
     return res.download(__dirname + '../cracked.txt')
 })
 
-module.exports = router
+module.exports = router 
