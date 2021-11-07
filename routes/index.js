5156452bfd4b456ec3cf5eed84648d911a9f814a
Kobi <shotrz@gmail.com>
2021-11-07 09:03:11 +0200

    update log path

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 const router = require('express').Router()
+const path = require('path')
 
 router.use("/ping", (req, res) => {
     return res.send('pong')
 })
 router.get("/log", (req, res) => {
-    return res.download(__dirname + '../micro-hasher-process.log')
+    return res.download(path.join(__dirname, '../') + 'micro-hasher-process.log')
 })
 router.get("/cracked.txt", (req, res) => {
-    return res.download(__dirname + '../cracked.txt')
+    return res.download(path.join(__dirname, '../') + '../cracked.txt')
 })
 
 module.exports = router 
